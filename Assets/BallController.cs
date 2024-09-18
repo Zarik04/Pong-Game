1caa2d2bbc23ae86db54ad2ea38f776712f072cd
Zarshedjon Nasimov <2110058@newuu.uz>
2024-09-18 19:47:16 +0500

    Applied bounce function to the ball.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -31,4 +31,40 @@ public class BallController : MonoBehaviour
     {
         rb.velocity = new Vector2(-1, 0) * (initialSpeed + speedIncrease * hitCounter);
     }
+
+    private void PlayerBounce(Transform obj) 
+    {
+        hitCounter++;
+
+        Vector2 ballPosition = transform.position;
+        Vector2 playerPosition = obj.position;
+
+        float xDirection;
+        float yDirection;
+
+        // flipping the direction once ball hits the edge
+        if (transform.position.x > 0) {
+            xDirection = -1;
+        } else {
+            xDirection = 1;
+        }
+
+        // avoiding to get stuck
+        yDirection = (ballPosition.y - playerPosition.y)/obj.GetComponent<Collider2D>().bounds.size.y;
+
+        if (yDirection == 0) {
+            yDirection = 0.25f;
+        }
+
+        // applying new directions to the ball
+        rb.velocity = new Vector2(xDirection, yDirection) * (initialSpeed + (speedIncrease * hitCounter));
+    }
+
+    // calling bounce function when ball hits the one of the paddles
+    private void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.name == "PaddleA" || other.gameObject.name == "PaddleB") {
+            PlayerBounce(other.transform);
+        }
+    } 
 }
